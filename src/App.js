0c2e8483d51bd525f9a3f75c412a1ed90a58eb22
Jeena <jmj.jeena@gmail.com>
2018-10-17 20:54:33 -0700

    added contents to Book and installed prop-type

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Search from './components/Search'
 import BookShelf from './components/BookShelf'
 import Book from './components/Book'
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Book extends React.Component {
-    state = {}
-    render() {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        return (
+            < div className="book" >
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.image}")` }}></div>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">{this.props.author}</div>
+            </div >
+        );
     }
 }
-export default Book
+
+Book.propTypes = {
+    // You can declare that a prop is a specific JS primitive. By default, these
+    // are all optional.
+    title: PropTypes.string,
+    author: PropTypes.string,
+    image: PropTypes.string
+}
+
+
+
+
+export default Book;
